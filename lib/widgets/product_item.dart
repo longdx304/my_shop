226dab51d3532be3c72f3ce34a8459ac72c15988
Long Do <longdo@dobtech.vn>
2021-01-01 18:56:22 +0700

    connect product detail screen with products provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -19,8 +19,10 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          onTap: () =>
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName),
+          onTap: () => Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,
+            arguments: id,
+          ),
           child: Image.network(
             imageUrl,
             fit: BoxFit.cover,
