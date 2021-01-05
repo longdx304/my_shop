ae8667844f195405ec51fd47ab4d50ad6035fcd1
Long Do <longdo@dobtech.vn>
2021-01-05 22:38:22 +0700

    can now expand order to view products ordered

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,26 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as orderProvider;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final orderProvider.OrderItem order;
 
   const OrderItem(this.order);
 
   @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
+    var products = widget.order.products;
+
     return Card(
       margin: EdgeInsets.all(10),
-      child: ListTile(
-        title: Text('\$${order.amount.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: () {},
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () => setState(() => _expanded = !_expanded),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(products.length * 20.0 + 10, 180),
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      products[index].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${products[index].quantity}x \$${products[index].price}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
