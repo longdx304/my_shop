dc2d84c0a17c6c609b5c1ea4ff3059bd3333a146
Long Do <longdo@dobtech.vn>
2021-01-11 14:29:47 +0700

    add auth screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'screens/cart_screen.dart';
 import 'screens/orders_screen.dart';
 import 'screens/user_products_screen.dart';
 import 'screens/edit_product_screen.dart';
+import 'screens/auth_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           EditProductScreen.routeName: (_) => EditProductScreen(),
           UserProductsScreen.routeName: (_) => UserProductsScreen(),
