f8399b0db2ccbf6e64eceb1afbd9c4cf90fedfb7
Long Do <longdo@dobtech.vn>
2021-01-13 21:50:46 +0700

    added animation for expanding order

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -21,27 +21,30 @@ class _OrderItemState extends State<OrderItem> {
   Widget build(BuildContext context) {
     var products = widget.order.products;
 
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? min(products.length * 20.0 + 102, 192) : 92,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () => setState(() => _expanded = !_expanded),
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () => setState(() => _expanded = !_expanded),
-            ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(
                 horizontal: 15,
                 vertical: 4,
               ),
-              height: min(products.length * 20.0 + 10, 180),
+              height: _expanded ? min(products.length * 20.0 + 10, 100) : 0,
               child: ListView.builder(
                 itemCount: products.length,
                 itemBuilder: (context, index) => Row(
@@ -65,7 +68,8 @@ class _OrderItemState extends State<OrderItem> {
                 ),
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
