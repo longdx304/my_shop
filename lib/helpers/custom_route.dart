42ca6d1073d439a48ac6c37c802030f5051b9f46
Long Do <longdo@dobtech.vn>
2021-01-14 22:15:53 +0700

    change all route transition to fadein

--- lib/helpers/custom_route.dart
+++ lib/helpers/custom_route.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomRoute extends MaterialPageRoute {
+class CustomRoute<T> extends MaterialPageRoute<T> {
   CustomRoute({builder, settings})
       : super(builder: builder, settings: settings);
 
@@ -12,6 +12,23 @@ class CustomRoute extends MaterialPageRoute {
     Widget child,
   ) {
     if (settings.name == '/') return child;
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
+
+class CustomPageTransitionBuilder extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    if (route.settings.name == '/') return child;
     return FadeTransition(
       opacity: animation,
       child: child,
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/helpers/custom_route.dart';
 import 'package:my_shop/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -67,6 +68,12 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.purple,
               accentColor: Colors.deepOrange,
               fontFamily: 'Lato',
+              pageTransitionsTheme: PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android: CustomPageTransitionBuilder(),
+                  TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                },
+              ),
             ),
             home: ifAuth(ProductsOverviewScreen()),
             routes: {
